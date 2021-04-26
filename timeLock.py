56912d350bcf0ea1f39d6c1d90bb1ffb9320b5e0
eddieredmann3 <eddieredmann3@gmail.com>
2021-04-26 15:11:58 -0500

    Update timeLock.py

--- timeLock.py
+++ timeLock.py
@@ -3,29 +3,64 @@ from sys import *
 from hashlib import *
 
 def time_differential(ep, curr):
-    ep_t_struct = strp_time(ep, "%Y %m %d %H %M %S")
+    #makes the epoch time that we read from stdin a usable form
+    ep_t_struct = strptime(ep, "%Y %m %d %H %M %S")
     
-    encodedValue = md5(md5())
+    #gets the seconds so that we only change the code once per minute
+    ep_t_sec = ep_t_struct.tm_sec
+    curr_t_sec = curr.tm_sec
+    
+    #makes the struct_times into floating point seconds since the true epoch
+    ep_t = mktime(ep_t_struct)
+    curr_t = mktime(curr)
+    
+    #sets the second offset so that we only change the code once per minute
+    if curr_t_sec > ep_t_sec:
+        sec_diff = curr_t_sec > ep_t_sec
+    else:
+        sec_diff = curr_t_sec + (60 - ep_t_sec)
+    
+    #returns the elapsed time
+    return ((curr_t - ep_t) - sec_diff)
+    
+def hash_code_get(time_diff):
+    time_diff = str(time_diff)
+    
+    #uses the hashlib md5 function to convert the elapsed time string to a hash
+    temp_hash = md5(time_diff.encode())
+    #then it actually turns it into a hexadecimal hash
+    temp_hash = temp_hash.hexdigest()
+    
+    #repeating the process on the already hashed function
+    hash_val = md5(temp_hash.encode())
+    hash_val = hash_val.hexdigest()
+    
+    #generating the code
+    code = hash_val[0:2] + hash_val[-2:]
+    
+    return code
 
-    
 # we can find the epoch using the following commmand according to the time library documentation
 epoch = ""
 for line in stdin:
+    #reads the epoch from the epoch file or stdin
     if 'Exit' == line.rstrip():
         break
     epoch += line
 
-#print(epoch)
 # False for normal operation, True for debugging
-debug = True
 
+debug = False
 # When true, time will be set manually
-if(debug == True):
-    # MUST SET MANUAL VALUE WHEN USING DEBUG MODE
-    curr_time = "2013 05 06 07 43 25"
-
-# When false, time will use system time.
+if debug == True:
+    curr_time = "2017 03 23 18 02 06"
+    curr_time_s = strptime(curr_time, "%Y %m %d %H %M %S")
+    t = time_differential(epoch, curr_time)
+    print(hash_code_get(t) + "\n")
+    print("current system time: " + curr_time)
 else:
-    temp_time = localtime()
-    curr_time = strftime("%Y %m %d %H %M %S", temp_time)
-    print(curr_time)
+    curr_time = localtime()
+    curr_time_s = strftime("%Y %m %d %H %M %S", curr_time)
+    t = time_differential(epoch, curr_time)
+    print(hash_code_get(t) + "\n")
+    print("current system time: " + curr_time_s)
