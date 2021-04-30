ae5e711def16c33f699738d62ad10a20c27007f7
tsschwartz99 <56732424+tsschwartz99@users.noreply.github.com>
2021-04-30 15:34:34 -0500

    Update steg.py

--- steg.py
+++ steg.py
@@ -1,3 +1,6 @@
+import sys
+from sys import stdout
+
 
 W = 
 H = 
@@ -54,3 +57,43 @@ def direct():
 				offset += interval
 		H += b
 		offset += interval
+
+
+###################### ENTRY POINT ##########################
+# Will take in command-line arguments
+
+if(sys.__stdin__.isatty()):
+    # if store option
+    if(sys.argv[1] == "-s"):
+        stdout.write("STORE\n")
+        stdout.flush()
+    # if retrieve option
+    elif(sys.argv[1] == "-r"):
+        stdout.write("RETRIEVE\n")
+        stdout.flush()
+    # if bit option
+    if(sys.argv[2] == "-b"):
+        stdout.write("BIT\n")
+        stdout.flush()
+    # if byte option
+    elif(sys.argv[2] == "-B"):
+        stdout.write("BYTE\n")
+        stdout.flush()
+    # get the offset value
+    print(sys.argv[3][2:])
+    # check to see if there is a given interval
+    if(sys.argv[4][0:2] == "-i"):
+        print("INTERVAL = " + sys.argv[4][2:])
+    # interval not given so sys.argv[4] is -w
+    elif(sys.argv[4][0:2] == "-w"):
+        print("INTERVAL = 1")
+        print("WRAPPER FILE = " + sys.argv[4][2:])
+    # see if argv[5] is -w or -h or null
+    if(len(sys.argv) >= 6):
+        if(sys.argv[5][0:2] == "-w"):
+            print("WRAPPER FILE = " + sys.argv[5][2:])
+        elif(sys.argv[5][0:2] == "-h"):
+            print("HIDDEN FILE = " + sys.argv[5][2:])
+    # sys.argv[6] is -h
+    if(len(sys.argv) == 7):
+        print("HIDDEN FILE = " + sys.argv[6][2:])
