b1676dbf092487163e1df25f8bb1acd2fcaad734
eddieredmann3 <eddieredmann3@gmail.com>
2021-05-04 11:35:39 -0500

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,3 +1,4 @@
+
 ###########################################################################
 # The Magician
 # Alex Davis, Landon Jackson, Skyler McAffry, Mary Nations, Matt Post,
@@ -10,22 +11,16 @@ def read_file(infile):
     # to covnvert that to a list of integers, which we can
     # perform the xor operation on
     char_vals = [] 
-    chars = []
-    fb = infile.read(1) # reads the first byte
-    fb = ord(fb) #gives the UTF-8 value of that byte
-    char_vals.append(fb) #appends it to char_vals
-    
-    while fb != "":
-        try:
-            fb = infile.read(1)
-            fb = ord(fb)
-            # print(chr(fb))
-            char_vals.append(fb)
-        # ord() throws a TypeError when it reaches the end of the file
-        # because it's trying to perform ord(b'')
-        except TypeError:
+
+    pos = 0
+    while True:
+        fb = infile.read(1)
+        if not fb:
             break
-    #print(chars)
+        pos += 1
+        infile.seek(pos)
+        fb = ord(fb)
+        char_vals.append(fb)
     return char_vals
 
 def xor(msg, key):
@@ -43,23 +38,23 @@ def xor(msg, key):
                 if (len(key_v) == len(msg)):
                     break
                 key_v.append(k)
+        
     for i in range(len(key_v)):
-        xc = msg[i]^key_v[i] # '^' is the python bitwise xor operator 
+        xc = msg[i]^key_v[i] # '^' is the python bitwise xor operator
+        xc = xc.to_bytes(1, "big")
         xorlist.append(xc)
     return xorlist
 
 def convert_message(x_list):
-    message = ""
+    #message = ""
     #takes the xor list of characters and creates the message
     for char in x_list:
-        x = chr(char)
-        message += x
-    return message
+        stdout.buffer.write(char)
 
 data = stdin.buffer
 
 #gets the key file and reads its contents as bytes
-k = "key"
+k = "key2"
 f = open(k, "rb")
 msg = read_file(data)
 key = read_file(f)
@@ -67,4 +62,5 @@ f.close()
 data.close()
 xor_vals = xor(msg, key)
 
-print(convert_message(xor_vals))
+#print(xor_vals[:10])
+convert_message(xor_vals)
